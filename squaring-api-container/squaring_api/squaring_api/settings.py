a9eb42b241798fcc69cc967e05173cffe3c05e3e
Christopher Schreiber <pchristopherschreiber@gmail.com>
2022-11-13 03:52:20 -0500

    removed django key from settings

--- squaring-api-container/squaring_api/squaring_api/settings.py
+++ squaring-api-container/squaring_api/squaring_api/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sqib&^#^6p861kikzq0q!s7l&kzc23+rpqi1046fsiw)d0d#laa+i
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
